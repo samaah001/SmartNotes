c6e686c727c7b7131eaf086632f15aca26e746c9
unknown <miraamir189@gmail.com>
2021-05-26 16:34:32 +0530

    First comment

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-
+import { PhotoserviceService } from '../photoservice.service';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
-
+  constructor(public photoservice: PhotoserviceService) {}
+  addPhotoToGallery() {
+    this.photoservice.addNewToGallery();
+  }
 }
---
+++ src/app/photoservice.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {
+  Camera,
+  CameraResultType,
+  CameraSource,
+  Photo,
+} from '@capacitor/camera';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { Storage } from '@capacitor/storage';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PhotoserviceService {
+  constructor() {}
+  public async addNewToGallery() {
+    // Take a photo
+    const capturedPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100,
+    });
+  }
+}
